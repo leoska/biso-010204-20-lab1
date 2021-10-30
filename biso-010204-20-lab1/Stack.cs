6f565de29d73c454d3721b921710666acb39e6b5
zhidkov.georg <zhidkov.georg@gmail.com>
2021-10-30 14:24:10 +0300

    added get, set, print

--- biso-010204-20-lab1/Stack.cs
+++ biso-010204-20-lab1/Stack.cs
@@ -22,5 +22,34 @@ namespace biso_010204_20_lab1
         {
             return stack.Count;
         }
+
+        public int Get(int pos)
+        {
+            Stack tmp = new Stack();
+            for (int i = Count() - 1; i > pos; i--)
+                tmp.Push(Pop());
+            int result = stack[Count() - 1];
+            for (int i = tmp.Count() - 1; i >= 0; i--)
+                Push(tmp.Pop());
+
+            return result;
+        }
+
+        public void Set(int pos, int value)
+        {
+            Stack tmp = new Stack();
+            for (int i = Count() - 1; i > pos; i--)
+                tmp.Push(Pop());
+            stack[Count() - 1] = value;
+            for (int i = tmp.Count() - 1; i >= 0; i--)
+                Push(tmp.Pop());
+        }
+
+        public void Print()
+        {
+            foreach (int i in stack)
+                Console.WriteLine(i);
+            Console.WriteLine("________");
+        }
     }
 }

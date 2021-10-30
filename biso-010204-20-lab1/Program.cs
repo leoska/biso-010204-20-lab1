3ce55ee2c95a0c99e0a34679a3727c330d5c1058
Neyasyti <alexla5fn@gmail.com>
2021-10-30 14:37:43 +0300

    rnd

--- biso-010204-20-lab1/Program.cs
+++ biso-010204-20-lab1/Program.cs
@@ -8,7 +8,16 @@ namespace biso_010204_20_lab1
         {
             // Стэк на массиве с пузырьковой сортировкой
             // Стэк пишем в отдельном классе
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+            Stack stack = new Stack();
+            Random rnd = new Random();
+            for (int i = 0; i < 10; i++)
+                stack.Push(rnd.Next(1, 100));
+            stack.Print();
+
+
+
+
         }
     }
 }

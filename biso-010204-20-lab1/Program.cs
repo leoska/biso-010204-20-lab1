386813c83e69969de613c7be3ad713404daee302
Андрей Комков <mister.diamand@yandex.ru>
2021-10-30 14:57:12 +0300

    add sort

--- biso-010204-20-lab1/Program.cs
+++ biso-010204-20-lab1/Program.cs
@@ -15,9 +15,26 @@ namespace biso_010204_20_lab1
                 stack.Push(rnd.Next(1, 100));
             stack.Print();
 
+            for (int i = stack.Count() - 1; i > 0; --i) {
+                bool flag = false;
 
+                for (int j = 0; j < i; j++)
+                {
+                    if (stack.Get(j) > stack.Get(j + 1))
+                    {
+                        int tmp = stack.Get(j);
+                        stack.Set(j, stack.Get(j + 1));
+                        stack.Set(j + 1, tmp);
+                        flag = true;
 
+                    }
+                }
+                if (!flag)
+                    break;
+            }
+            stack.Print();
 
         }
+
     }
 }

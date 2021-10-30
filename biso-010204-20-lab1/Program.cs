6b736a5918ca884089707c8f5fbc3af2ff0be0b2
leoska <leo77551@gmail.com>
2021-10-30 14:01:12 +0300

    first commit

---
+++ biso-010204-20-lab1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace biso_010204_20_lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Стэк на массиве с пузырьковой сортировкой
+            // Стэк пишем в отдельном классе
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
